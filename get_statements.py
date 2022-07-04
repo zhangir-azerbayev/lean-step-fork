fc101331518986e38f223026c54d1ffe6c670b10
Zhangir Azerbayev <zazerbayev@gmail.com>
2022-07-04 18:09:29 -0400

    almost works, i think

--- get_statements.py
+++ get_statements.py
@@ -2,14 +2,36 @@ import jsonlines
 import re 
 import sys 
 
+
 def add_hyp(stmnt, hyp): 
     if len(stmnt[stmnt.rfind("\n"):] + hyp) > 80:
         new_stmnt = stmnt + "\n\t" + hyp
         return new_stmnt
     else: 
         return stmnt + " " + hyp
+def get_paren_type(hyp, tp): 
+    print("hypothesis: ", hyp)
+    key = r"((\s)|(\()|(\{)|(\[))" + f"{re.escape(hyp[0])}.*?: {re.escape(hyp[1])}"
+    print("first key: ", key)
+    first_search = re.search(key, tp)
+    if first_search is None: 
+        print("no match found, return: (")
+        return "("
+    else: 
+        print("first match: ", first_search.group())
+        i = first_search.span()[0]+1
+        left = tp[:i]
+        print("left: ", left)
 
-TYPE_STAR_PATTERN = "Type u_[0-9][0-9]*"
+        j_1 = left.rfind("(")
+        j_2 = left.rfind("{")
+        j_3 = left.rfind("[")
+        j = max(j_1, j_2, j_3)
+
+        print("result: ", left[j])
+        return left[j]
+
+TYPE_STAR_PATTERN = "Type u_.*"
 
 path = sys.argv[1]
 
@@ -32,22 +54,32 @@ for step in data:
                 statement = add_hyp(statement, f"[{to_add_tp}]")
                 i += 1
             else: 
+                paren_type = get_paren_type(hyps[i], step["decl_tp"])
+
                 if re.match(TYPE_STAR_PATTERN, hyps[i][1]): 
-                    hyps[i][1] = "Type*"
                     i_range = i+1 
-                    while i_range < len(hyps) and re.match(TYPE_STAR_PATTERN, hyps[i_range][1]): 
+                    while i_range < len(hyps) and re.match(TYPE_STAR_PATTERN, hyps[i_range][1]) and paren_type==get_paren_type(hyps[i_range], step["decl_tp"]):  
                         i_range += 1
+                    hyps[i][1]="Type*"
                 else: 
                     i_range = i+1
-                    while i_range<len(hyps) and hyps[i][1]==hyps[i_range][1]:
+                    while i_range<len(hyps) and hyps[i][1]==hyps[i_range][1] and paren_type==get_paren_type(hyps[i_range], step["decl_tp"]):
                         i_range += 1
                 
+
                 to_add_nm = " ".join([h[0] for h in hyps[i:i_range]])
                 to_add_tp = hyps[i][1]
-                statement = add_hyp(
-                        statement, 
-                        f"({to_add_nm} : {to_add_tp})"
-                        )
+
+                if paren_type=="(":
+                    statement = add_hyp(
+                            statement, 
+                            f"({to_add_nm} : {to_add_tp})"
+                            )
+                else: 
+                    statement = add_hyp(
+                            statement, 
+                            f"{{{to_add_nm} : {to_add_tp}}}"
+                            )
                 i = i_range
         
     
@@ -62,7 +94,7 @@ for step in data:
 
         statement += " :\n\t" + conc
 
-        print("#"*40 + "\n" + step["decl_tp"] + statement)
+        print("\n" + step["decl_tp"] + statement+"\n" + "#"*40)
 
 
 
